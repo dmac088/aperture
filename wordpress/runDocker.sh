5c008413eafb193a55328ce3960bda59ae7339fc
Dan <danielmackie82@gmail.com>
2022-07-15 14:53:55 +0800

    asdf

--- wordpress/runDocker.sh
+++ wordpress/runDocker.sh
@@ -3,7 +3,7 @@
 docker run -d \
            --name my-wordpress-container  \
 	       -p 8001:80 \
-           -e WORDPRESS_CONFIG_EXTRA="define('WP_HOME','https://www.littlebagshop.com'); define('WP_SITEURL','https://www.littlebagshop.com');" \
+           -e WORDPRESS_CONFIG_EXTRA="define('WP_HOME','https://www.sv2.io'); define('WP_SITEURL','https://www.sv2.io');" \
            -e WORDPRESS_DB_HOST=my-wordpressdb-container:3306 \
            -e WORDPRESS_DB_USER=wordpress \
            -e WORDPRESS_DB_PASSWORD=wordpress \
